afa225386702d7863378ca4250ed636ce60ceb4b
Alex Georgiev <alexander.georgiev@source-weave.com>
2018-03-08 19:16:44 +0200

    chore: minor changes and comment additions

--- ASP.NETCoreAPIAuthSchemeUsingJWTs/Controllers/ApiControllers/TestAuthController.cs
+++ ASP.NETCoreAPIAuthSchemeUsingJWTs/Controllers/ApiControllers/TestAuthController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NETCoreAPIAuthSchemeUsingJWTs.Controllers.ApiControllers
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Produces("application/json")]
     [Route("api/TestAuth")]
     public class TestAuthController : Controller
     {
+        [HttpGet]
+        [Route("example")]
+        public IActionResult GetExampleValues()
+        {
+            var testResult = "You have been successfully authorized via a bearer token!";
+
+            return new ObjectResult(testResult);
+        }
     }
 }
