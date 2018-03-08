2cb884c40520307d2b4c3d5a9d15a0d2a0b12c21
Alex Georgiev <alexander.georgiev@source-weave.com>
2018-03-08 18:50:03 +0200

    change: configureservices startup.cs method for bearer token authenticatio and authorization

--- ASP.NETCoreAPIAuthSchemeUsingJWTs/Startup.cs
+++ ASP.NETCoreAPIAuthSchemeUsingJWTs/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Text;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
@@ -7,11 +8,16 @@ using Microsoft.Extensions.DependencyInjection;
 using ASP.NETCoreAPIAuthSchemeUsingJWTs.Data;
 using ASP.NETCoreAPIAuthSchemeUsingJWTs.Models;
 using ASP.NETCoreAPIAuthSchemeUsingJWTs.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ASP.NETCoreAPIAuthSchemeUsingJWTs
 {
     public class Startup
     {
+        private const string SecretKey = "don't_reveal_me_to_anybody_in_production";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,6 +28,28 @@ namespace ASP.NETCoreAPIAuthSchemeUsingJWTs
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser().Build());
+            });
+
+            services.AddAuthentication()
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+
+                    cfg.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = "TokenApiAuthenticationGuide",
+                        ValidAudience = "Client consuming the API",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey))
+                    };
+
+                });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection")));
 
